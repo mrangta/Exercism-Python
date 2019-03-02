f5b66a32cf8d3cd98a88dd2ea9b835e95be8589a
Monika <monika@ubuntu.ubuntu-domain>
2019-03-03 01:07:55 +0200

    add variable value to a string dynamically

---
+++ two-fer/two_fer.py
@@ -0,0 +1,7 @@
+def two_fer(name = 'you'):
+    if name =='you':
+        return "One for you, one for me."
+    else:
+        return "One for " + name + ", one for me."
+
+#        return "One for {}, one for me.".format(name)
